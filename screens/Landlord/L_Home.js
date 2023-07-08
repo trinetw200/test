7df818f878b5b32ca9a49948ce989ea6fbe13f4a
trinetw200 <trinetw200@users.noreply.github.com>
2023-07-08 11:45:10 +0800

    home buttom bar

---
+++ screens/Landlord/L_Home.js
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons,FontAwesome5,Ionicons   } from '@expo/vector-icons'; 
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="HouseEdit" component={HomeScreen} options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home-edit" size={24} color="black" />
+          ),
+        }}/>
+      <Tab.Screen name="Calendar" component={SettingsScreen} options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="calendar-alt" size={24} color="black" />
+          ),
+        }} />
+        <Tab.Screen name="Home" component={SettingsScreen} options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home-search" size={24} color="black" />
+          ),
+        }} />
+        <Tab.Screen name="PaymentRecord" component={SettingsScreen} options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="text-box-check-outline" size={24} color="black" />
+          ),
+        }} />
+        <Tab.Screen name="profile" component={SettingsScreen} options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-circle" size={24} color="black" />
+          ),
+        }} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
